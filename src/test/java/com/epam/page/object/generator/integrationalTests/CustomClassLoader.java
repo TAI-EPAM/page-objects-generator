de099ba55307b276ba451e9c0acf92a5aad6d063
yuriy <knifeparty4@gmail.com>
2017-12-08 15:42:39 +0300

    Added our own classloader; Tests can be formed automaticly now (only need to add url to properties file); Finally got working version (maybe need some refactoring)

--- src/test/java/com/epam/page/object/generator/integrationalTests/CustomClassLoader.java
+++ src/test/java/com/epam/page/object/generator/integrationalTests/CustomClassLoader.java
@@ -5,12 +5,16 @@ import java.io.InputStream;
 
 public class CustomClassLoader extends ClassLoader {
 
+private static final String CLASS_EXTENSION = ".class";
+private static final String TEST_PACKAGE = "test";
+private static final String MANUAL_PACKAGE = "manual";
+
     @Override
     public Class<?> loadClass(String name) throws ClassNotFoundException {
-        if (name.startsWith("test.") || name.startsWith("manual.")) {
+        if (name.startsWith(TEST_PACKAGE) || name.startsWith(MANUAL_PACKAGE)) {
             try {
                 InputStream is = getParent().getResourceAsStream(
-                        name.replace('.', '/') + ".class"
+                        name.replace('.', '/') + CLASS_EXTENSION
                 );
                 byte[] buf = new byte[10000];
                 int len = is.read(buf);
