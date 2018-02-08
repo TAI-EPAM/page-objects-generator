9c2ce646f2125323b23d9b9c567d686e33dbb59d
bezmoshyk@yandex.ru <Alex_@0th>
2018-02-08 16:27:35 +0300

    Reformat checkInnerSearch method

--- src/main/java/com/epam/page/object/generator/util/SearchRuleExtractor.java
+++ src/main/java/com/epam/page/object/generator/util/SearchRuleExtractor.java
@@ -4,11 +4,14 @@ import com.epam.page.object.generator.model.Selector;
 import com.epam.page.object.generator.model.searchrule.ComplexInnerSearchRule;
 import com.epam.page.object.generator.model.searchrule.ComplexSearchRule;
 import com.epam.page.object.generator.model.searchrule.SearchRule;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import us.codecraft.xsoup.Xsoup;
 
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -16,13 +19,15 @@ import java.util.List;
  */
 public class SearchRuleExtractor {
 
+    private final static Logger logger = LoggerFactory.getLogger(SearchRuleExtractor.class);
+
     public SearchRuleExtractor() {
     }
 
     /**
      * This method returns all inner {@link Elements} according to {@link SearchRule}.
      *
-     * @param element    {@link Element} that we are extracting from {@link Elements}.
+     * @param element {@link Element} that we are extracting from {@link Elements}.
      * @param searchRule {@link SearchRule} is a rule that get suitable {@link Selector}.
      * @return {@link Elements} from {@link Element} according to {@link SearchRule}
      */
@@ -48,19 +53,12 @@ public class SearchRuleExtractor {
         List<ComplexInnerSearchRule> rules = ((ComplexSearchRule) searchRule).getInnerSearchRules();
 
         for (Element element : elements) {
-            Element document = element.parent();
-            try {
-                rules = rules.subList(1, rules.size());
-            } catch (IndexOutOfBoundsException e) {
-                break;
-            }
-            Iterator<ComplexInnerSearchRule> iterator = rules.iterator();
-
-            while (iterator.hasNext()) {
-                Elements innerElements = extractElementsFromDocument(document, iterator.next());
-                if (innerElements.size() == 0) {
-                    iterator.remove();
-                }
+            if (rules.size() > 1) {
+                List<ComplexInnerSearchRule> removedRules = rules.subList(1, rules.size()).stream()
+                    .filter(complexInnerSearchRule -> extractElementsFromDocument(element.parent(),
+                        complexInnerSearchRule).isEmpty()).collect(Collectors.toList());
+                logger.warn("Incorrect search rules: " + removedRules.toString());
+                rules.removeAll(removedRules);
             }
         }
     }
